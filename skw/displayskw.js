6bbdc917e69f42af2f6d7fa86d74d6a263c09fcc
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-04-03 20:42:52 +0700

    Create displayskw.js

---
+++ skw/displayskw.js
@@ -0,0 +1,25 @@
+const chalk = require('chalk');
+
+const welcomeskw = chalk.hex('#00CED1')(`
+   ███████╗██╗  ██╗██╗    ██╗
+   ██╔════╝██║ ██╔╝██║    ██║
+   ███████╗█████╔╝ ██║ █╗ ██║
+   ╚════██║██╔═██╗ ██║███╗██║
+   ███████║██║  ██╗╚███╔███╔╝
+   ╚══════╝╚═╝  ╚═╝ ╚══╝╚══╝ 
+`);
+
+function displayskw() {
+  console.log(welcomeskw);
+  console.log(chalk.hex('#00CED1')(" ╔══════════════════════════════════════════════════════════════╗"));
+  console.log(chalk.hex('#00CED1')(" ║ ≣  Fitur Autobot by SKW AIRDROP HUNTER                       ║"));
+  console.log(chalk.hex('#00CED1')(" ║══════════════════════════════════════════════════════════════║"));
+  console.log(chalk.hex('#00CED1')(" ║ ➤   Auto claim Daily                                        ║"));
+  console.log(chalk.hex('#00CED1')(" ║ ➤   Auto Ping                                               ║"));
+  console.log(chalk.hex('#00CED1')(" ║ ➤   Multi Akun                                              ║"));
+  console.log(chalk.hex('#00CED1')(" ║ ➤   Sudah Pasti Elig                                        ║"));
+  console.log(chalk.hex('#00CED1')(" ╚══════════════════════════════════════════════════════════════╝"));
+  console.log(chalk.hex('#00CED1')("   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"));
+}
+
+module.exports = { displayskw };
